af7c0ad3904cd508f30280130eb108b48e3bfc63
Jaron D Evans <jarondevans@gmail.com>
2018-08-16 23:28:00 +0000

    email functionality

---
+++ app/mailers/order_mailer.rb
@@ -0,0 +1,12 @@
+class OrderMailer < ApplicationMailer
+
+  default from: 'no-reply@jungle.com'
+
+  def order_email(order)
+    @order = order
+    # @user = params[:user]
+    # @url  = 'http://localhost:3000/orders/3'
+    mail(to: @order.email, subject: @order.id)
+  end
+end
+
--- app/models/order.rb
+++ app/models/order.rb
@@ -7,4 +7,10 @@ class Order < ActiveRecord::Base
 
   validates :stripe_charge_id, presence: true
 
+  after_create :order_receipt_email
+
+  def order_receipt_email
+    OrderMailer.order_email(self).deliver_now
+  end
+
 end
